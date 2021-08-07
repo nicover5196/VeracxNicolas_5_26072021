d0977ce2e2fa62c831a25f5d354f775b2a414b60
nicover5196 <76521608+nicover5196@users.noreply.github.com>
2021-08-07 21:34:24 +0200

    modif des fichiers

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -1,20 +1,20 @@
 alert("page produit")
 
-const url = "http://localhost:3000/api/teddies";
+// Récuperer l'id qui a été passée en parametre de mon url
 
-// Récupération des données avec Fetch
-fetch(url)
-    .then(resultat => resultat.json().then(data => {
-            // Afficher mes données dans la console
-            console.log(data)
-        })
-        // En cas d'erreur, afficher celle ci en console
-    ).catch(err => console.log('Erreur :' + err));
+const urlId = window.location.search;
 
+// Affichage dans la console 
+console.log(urlId)
 
-// Récuperer l'id qui a été passée en parametre de mon url 
+// Récupérer la valeur du paramètre
+
+const urlParams = new URLSearchParams(urlId);
+
 // Faire un fetch pour recuperer les infos de mon produit
-// Afficher les infos de mon produit
+
+
+
 // Précisez les options de mon produit dans un selecteur ( couleur..)
 // au clic d'un boutton créer, ajouter mon produit dans le local storage
 
